00d4a2b3cf600fcaf716819956ded5a8f340a8b0
Ryan Truran <ryantruran@gmail.com>
2020-10-31 18:29:01 -0500

    init

---
+++ main.py
@@ -0,0 +1,77 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import os
+import tarfile
+import urllib.request
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from pandas.plotting import scatter_matrix
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+# Press the green button in the gutter to run the script.
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+def plot_housing_data(housing):
+    housing.hist(bins=50, figsize=(20,15))
+    plt.show()
+
+if __name__ == '__main__':
+    housing = load_housing_data()
+    train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+    # print(housing.head())
+    # print(housing.info())
+    # print(housing['ocean_proximity'].value_counts())
+    # print(housing.describe())
+    housing["income_cat"] = pd.cut(housing["median_income"],
+                                   bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                                   labels=[1, 2, 3, 4, 5])
+
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(housing, housing["income_cat"]):
+        strat_train_set = housing.loc[train_index]
+        strat_test_set = housing.loc[test_index]
+
+    print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
+    for set_ in (strat_train_set, strat_test_set):
+        set_.drop("income_cat", axis=1, inplace=True)
+    housing = strat_train_set.copy()
+    housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1,
+                 s=housing["population"]/100, label="population", figsize=(10,7),
+                 c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,)
+    plt.show()
+    corr_matrix = housing.corr()
+    print(corr_matrix["median_house_value"].sort_values(ascending=False))
+    attributes = ["median_house_value", "median_income", "total_rooms",
+                  "housing_median_age"]
+    scatter_matrix(housing[attributes], figsize=(12, 8))
+    plt.show()
+
+    housing["rooms_per_household"] = housing["total_rooms"] / housing["households"]
+    housing["bedrooms_per_room"] = housing["total_bedrooms"] / housing["total_rooms"]
+    housing["population_per_household"] = housing["population"] / housing["households"]
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
